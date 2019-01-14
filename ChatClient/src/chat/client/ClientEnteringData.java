3b1e50513f842f939308e5a2143d6d431d76c0a5
Aleksey Goi <gm.goi.av@gmail.com>
2019-01-14 13:24:19 +0200

    new file:   ClientEnteringData.java

---
+++ ChatClient/src/chat/client/ClientEnteringData.java
@@ -0,0 +1,28 @@
+package chat.client;
+
+import network.TCPConnection;
+
+import java.util.Scanner;
+
+public class ClientEnteringData {
+
+    /**
+     * realized console entering data by client
+     */
+
+    public static void scannerRun(TCPConnection connection, String clientID){
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            if (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                //adding to our string client id
+                connection.sendString(line + clientID);
+                // if qqb was enter then exit application
+                if (line.equals("qqb")) {
+                    connection.disconnect();
+                    System.exit(0);
+                }
+            }
+        }
+    }
+}
